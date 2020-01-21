0ccde434315df3f411a5d009479164d5da477d28
Huy Dang <dangqhuy2411@gmail.com>
2020-01-21 14:57:46 +0700

    define style structures

--- src/main.c
+++ src/main.c
@@ -25,17 +25,67 @@ struct SheetData {
    XML_Char *sheet_id;
    char *path_name;
 };
+
+struct NumFMT {
+  XML_Char *format_code;
+  XML_Char *format_id;
+};
+
+struct Color{
+  XML_Char *rgb;
+};
+
+struct Font {
+  int size;
+  XML_Char *name;
+  int is_bold;
+  int is_italic; 
+  XML_Char *underline;
+  struct Color color;
+};
+
+struct Fill {
+  XML_Char *pattern_type;
+  struct Color bg_color;
+};
+
+struct Border{
+  XML_Char *style;
+  struct Color border_color;
+};
+
+struct BorderCell{
+  struct Border left;
+  struct Border right;
+  struct Border top;
+  struct Border bottom;
+};
+
 struct SheetData sheets_data[50];
+struct NumFMT numfmts[50];
+struct Font fonts[50];
+struct Fill fills[50];
+struct Border borders[50];
+
 int count_sheet = 0;
+int count_numFmt = 0;
 
 char *insert_substr_to_str_at_pos(char *des, char *substr, int pos){
-
+  char *_tmp_sheet_id = malloc(strlen(substr));
+  strcpy(_tmp_sheet_id, substr);
+  char *_tmp_path_name = malloc(strlen(_tmp_sheet_id) + strlen(des) + 1);
+  strncpy(_tmp_path_name, des, pos);
+  _tmp_path_name[pos] = '\0';
+  strcat(_tmp_path_name, _tmp_sheet_id);
+  strcat(_tmp_path_name, des + pos);
+  free(_tmp_sheet_id);
+  return _tmp_path_name;
 }
+
 static void XMLCALL
 startElement(void *userData, const XML_Char *name, const XML_Char **attrs) {
   int i;
   (void)attrs;
-  struct SheetData *sheets_data = userData;
 
   if (strcmp(name, "Override") == 0){
     for(i = 0; attrs[i]; i += 2){
@@ -45,6 +95,7 @@ startElement(void *userData, const XML_Char *name, const XML_Char **attrs) {
     }
   }
   if (strcmp(name, "sheet") == 0){
+    struct SheetData *sheets_data_callbackdata = userData;
     count_sheet++;
     for(i = 0; attrs[i]; i += 2){
       if(strcmp(attrs[i], "state") == 0 && strcmp(attrs[i + 1], "hidden") == 0){
@@ -52,35 +103,47 @@ startElement(void *userData, const XML_Char *name, const XML_Char **attrs) {
 	return;
       }
       if (strcmp(attrs[i], "name") == 0){
-	sheets_data[count_sheet - 1].name = malloc(strlen(attrs[i + 1]));
-	memcpy(sheets_data[count_sheet - 1].name, attrs[i + 1], strlen(attrs[i + 1]));
+	sheets_data_callbackdata[count_sheet - 1].name = malloc(strlen(attrs[i + 1]));
+	memcpy(sheets_data_callbackdata[count_sheet - 1].name, attrs[i + 1], strlen(attrs[i + 1]));
       }
       if (strcmp(attrs[i], "sheetId") == 0){
-	sheets_data[count_sheet - 1].sheet_id = malloc(strlen(attrs[i + 1]));
-
+	sheets_data_callbackdata[count_sheet - 1].sheet_id = malloc(strlen(attrs[i + 1]));
 	char *pattern_name = "xl/worksheets/sheet.xml";
-	char *_tmp_sheet_id = malloc(strlen(attrs[i + 1]));
-	int pos = 19; 
-	strcpy(_tmp_sheet_id, attrs[i + 1]);
-	sheets_data[count_sheet - 1].path_name = malloc(strlen(_tmp_sheet_id) + strlen(pattern_name) + 1);
-	strncpy(sheets_data[count_sheet - 1].path_name, pattern_name, pos);
-	sheets_data[count_sheet - 1].path_name[pos] = '\0';
-	strcat(sheets_data[count_sheet - 1].path_name, _tmp_sheet_id);
-	strcat(sheets_data[count_sheet - 1].path_name, pattern_name + pos);
-
-        free(_tmp_sheet_id);
-	memcpy(sheets_data[count_sheet - 1].sheet_id, attrs[i + 1], strlen(attrs[i + 1]));
+	sheets_data_callbackdata[count_sheet - 1].path_name = insert_substr_to_str_at_pos(pattern_name, attrs[i + 1], 19);
+	memcpy(sheets_data_callbackdata[count_sheet - 1].sheet_id, attrs[i + 1], strlen(attrs[i + 1]));
       }
 
     } 
   }
-  
+  if (strcmp(name, "numFmts")){
+  }
+  if (strcmp(name, "numFmt") == 0){
+    struct NumFMT *numFmts_callbackdata = userData;
+    count_numFmt++;
+    for (i = 0; attrs[i]; i += 2){
+      if (strcmp(attrs[i], "formatCode") == 0){
+        numFmts_callbackdata[count_numFmt - 1].format_code = malloc(strlen(attrs[i + 1]));
+	memcpy(numFmts_callbackdata[count_numFmt - 1].format_code, attrs[i + 1], strlen(attrs[i + 1]));
+      }
+      if (strcmp(attrs[i], "numFmtId") == 0){
+        numFmts_callbackdata[count_numFmt - 1].format_id = malloc(strlen(attrs[i + 1]));
+	memcpy(numFmts_callbackdata[count_numFmt - 1].format_id, attrs[i + 1], strlen(attrs[i + 1]));
+      }
+    }
+  }
+  if (strcmp(name, "font")){
+    for (i = 0; attrs[i]; i += 2) {
+    }
+  }
   /*printf("%" XML_FMT_STR "\n", name);*/
 }
 
 static void XMLCALL
 endElement(void *userData, const XML_Char *name) {
   (void)name;
+  if (strcmp(name, "numFmts") == 0){
+    userData = fonts;
+  }
 
 }
 
@@ -112,6 +175,18 @@ int load_workbook(zip_t *zip){
   }
   return status;
 }
+
+int load_styles(zip_t *zip){
+  const char *zip_file_name = "xl/styles.xml";
+  // Load NumFMT first
+  int status = process_zip_file(zip, zip_file_name, &numfmts);
+  for (int i = 0; i < count_numFmt; i++){
+    printf("Format code: %s\n", numfmts[i].format_code);
+    printf("Format id: %s\n", numfmts[i].format_id);
+  }
+  return status;
+}
+
 int load_sheet(zip_t *zip, const char *sheet_file_name){
   return 1;
 }
@@ -149,13 +224,24 @@ int process_zip_file(zip_t *zip, const char *zip_file_name, void *callbackdata){
 }
 
 int main(void){
-  const char *file_name = "/Volumes/PUBLIC/excelsample/report__codestringers.xlsx";
+  const char *file_name = "/home/huydang/Downloads/excelsample/report__codestringers.xlsx";
   zip_t *zip = open_zip(file_name);
   if (zip == NULL){
     fprintf(stderr, "File not found");
     return 0;
   }
   int status_workbook = load_workbook(zip);
+  if (!status_workbook){
+    fprintf(stderr, "Failed to read workbook");
+    zip_close(zip);
+    return 0;
+  }
+  int status_styles = load_styles(zip);
+  if (!status_styles){
+    fprintf(stderr, "Failed to read workbook");
+    zip_close(zip);
+    return 0;
+  } 
 
   zip_close(zip);
   return 0; 
