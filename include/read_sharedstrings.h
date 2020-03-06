7a8f00ea1ac26eec5526eceda9c5658888bf90da
Huy Dang <dangqhuy2411@gmail.com>
2020-03-06 18:41:04 +0700

    Done: Handle font data in sharedStrings

--- include/read_sharedstrings.h
+++ include/read_sharedstrings.h
@@ -1,9 +1,17 @@
-#include <zip.h>
-#include <expat.h>
+#ifndef INCLUDED_READ_SHAREDSTRINGS_H
+#define INCLUDED_READ_SHAREDSTRINGS_H
+
+#include <private.h>
 
 
+struct SharedStrings {
+  FILE *sharedStrings_file;
+  struct Font font;
+};
 
 extern XML_Parser xmlparser;
+struct Font new_font();
+
 void sharedStrings_main_start_element(void *, const XML_Char *, const XML_Char **);
 void sharedStrings_main_end_element(void *, const XML_Char *);
 void sharedStrings_lv1_start_element(void *userData, const XML_Char *name, const XML_Char **attrs);
@@ -14,3 +22,5 @@ void sharedStrings_lv2_end_element(void *userData, const XML_Char *name);
 void sharedStrings_rPritem_start_element(void *, const XML_Char *, const XML_Char **);
 void sharedStrings_rPritem_end_element(void *userData, const XML_Char *name);
 void sharedStrings_content_handler(void *, const XML_Char *, int);
+
+#endif
--- src/read_sharedstrings.c
+++ src/read_sharedstrings.c
@@ -1,162 +1,152 @@
 #include <read_sharedstrings.h>
 #include <string.h>
 
-struct Color {
-  XML_Char *rgb;
-};
 
-struct Font {
-  float sz;
-  XML_Char *name;
-  char isBold;
-  char isItalic;
-  XML_Char *underline;
-  struct Color color;
-};
 
-struct FontObj {
-  int has_font;
+struct Font new_font() {
   struct Font font;
-};
-
-struct FontObj new_fontobj() {
-  struct FontObj _fontobj;  
-  _fontobj.has_font = 0;
-  _fontobj.font.sz = 0.0;
-  _fontobj.font.isBold = '0';
-  _fontobj.font.isItalic = '0';
-  _fontobj.font.underline = malloc(1);
-  _fontobj.font.underline = '\0';
-  _fontobj.font.name = malloc(1);
-  _fontobj.font.name = '\0';
-  _fontobj.font.color.rgb = malloc(1);
-  _fontobj.font.color.rgb[0] = '\0';
-  return _fontobj;
+  font.sz = 0.0;
+  font.isBold = '0';
+  font.isItalic = '0';
+  font.underline = NULL;
+  font.name = NULL;
+  font.color.rgb = NULL;
+  return font;
 }
 
-struct FontObj fontobj;
+struct Font font;
 
-void sharedStrings_main_start_element(void *userData, const XML_Char *name, const XML_Char **attrs) {
+void sharedStrings_main_start_element(void *callbackdata, const XML_Char *name, const XML_Char **attrs) {
   (void)attrs;
-  fontobj= new_fontobj();
+
   if (strcmp(name, "sst") == 0) {
+    font = new_font();
     XML_SetElementHandler(xmlparser, sharedStrings_lv1_start_element, NULL);
   }
 }
 
-void sharedStrings_main_end_element(void *userData, const XML_Char *name) {
+void sharedStrings_main_end_element(void *callbackdata, const XML_Char *name) {
   if (strcmp(name, "sst") == 0) {
     XML_SetElementHandler(xmlparser, sharedStrings_main_start_element, NULL);
   }
 }
 
-void sharedStrings_lv1_start_element(void *userData, const XML_Char *name, const XML_Char **attrs) {
+void sharedStrings_lv1_start_element(void *callbackdata, const XML_Char *name, const XML_Char **attrs) {
   (void)attrs;
-  FILE *_tmp_sharedStrings_callbackdata = userData;
+  FILE *sharedStrings_file_callbackdata = callbackdata;
   if (strcmp(name, "si") == 0) {
-    fprintf(_tmp_sharedStrings_callbackdata, "<td>");
+    fprintf(sharedStrings_file_callbackdata, "<td>");
     XML_SetElementHandler(xmlparser, sharedStrings_lv2_start_element, sharedStrings_main_end_element);
   }
 }
 
-void sharedStrings_lv1_end_element(void *userData, const XML_Char *name) {
-  FILE *_tmp_sharedStrings_callbackdata = userData;
+void sharedStrings_lv1_end_element(void *callbackdata, const XML_Char *name) {
+  FILE *sharedStrings_file_callbackdata = callbackdata;
   if (strcmp(name, "si") == 0) {
-    fprintf(_tmp_sharedStrings_callbackdata, "</td>");
+    fprintf(sharedStrings_file_callbackdata, "</td>");
     XML_SetElementHandler(xmlparser, sharedStrings_lv1_start_element, sharedStrings_main_end_element);
   }
 }
 
-void sharedStrings_lv2_start_element(void *userData, const XML_Char *name, const XML_Char **attrs) {
+
+
+void sharedStrings_lv2_start_element(void *callbackdata, const XML_Char *name, const XML_Char **attrs) {
   (void)attrs;
+  FILE *sharedStrings_file_callbackdata = callbackdata;
   if (strcmp(name, "t") == 0) {
-    FILE *_tmp_sharedStrings_callbackdata = userData;
-    /*if (fontobj.has_font == 1) {
-        printf("---------------------------------------------\n");
-        printf("FONT SIZE: %f\n", fontobj.font.sz);
-        printf("FONT NAME: %s\n", fontobj.font.name);
-        printf("FONT IS BOLD: %c\n", fontobj.font.isBold);
-        printf("FONT IS ITALIC: %c\n", fontobj.font.isItalic);
-        printf("FONT UNDERLINE: %s\n", fontobj.font.underline);
-        printf("FONT COLOR RGB: %s\n", fontobj.font.color.rgb);
-    }*/
-    fprintf(_tmp_sharedStrings_callbackdata, "<p>");
-    free(fontobj.font.name);
-    free(fontobj.font.underline);
-    free(fontobj.font.color.rgb);
+    if (font.name != NULL) {
+      printf("---------------------------------------------\n");
+      printf("FONT SIZE: %f\n", font.sz);
+      printf("FONT NAME: %s\n", font.name);
+      printf("FONT IS BOLD: %c\n", font.isBold);
+      printf("FONT IS ITALIC: %c\n", font.isItalic);
+      printf("FONT UNDERLINE: %s\n", font.underline);
+      printf("FONT COLOR RGB: %s\n", font.color.rgb);
+    }
+    fprintf(sharedStrings_file_callbackdata, "<p>");
+    
+    if (font.name != NULL) {
+      free(font.name);
+    }
+    if (font.color.rgb != NULL) {
+      free(font.color.rgb);
+    }
+    if (font.underline != NULL) {
+      free(font.underline);
+    }
+    // Destroy font obj
+    font = new_font();
     XML_SetElementHandler(xmlparser, NULL, sharedStrings_lv2_end_element);
     XML_SetCharacterDataHandler(xmlparser, sharedStrings_content_handler);
   } else if(strcmp(name, "rPr") == 0) {
-    fontobj = new_fontobj();
-    fontobj.has_font = 1;
-    XML_SetElementHandler(xmlparser, sharedStrings_rPritem_start_element, NULL);
+    font = new_font();
+    XML_SetElementHandler(xmlparser, sharedStrings_rPritem_start_element, sharedStrings_rPritem_end_element);
     XML_SetCharacterDataHandler(xmlparser, NULL);
   }
 }
 
-void sharedStrings_lv2_end_element(void *userData, const XML_Char *name) {
+void sharedStrings_lv2_end_element(void *callbackdata, const XML_Char *name) {
   if (strcmp(name, "t") == 0) {
-    FILE *_tmp_sharedStrings_callbackdata = userData;
-    fprintf(_tmp_sharedStrings_callbackdata, "</p>");
+    FILE *sharedStrings_file_callbackdata = callbackdata;
+    fprintf(sharedStrings_file_callbackdata, "</p>");
     XML_SetElementHandler(xmlparser, sharedStrings_lv2_start_element, sharedStrings_lv1_end_element);
     XML_SetCharacterDataHandler(xmlparser, NULL);
-  } else {
+  } else if (strcmp(name, "rPr") == 0){
     XML_SetElementHandler(xmlparser, sharedStrings_lv2_start_element, sharedStrings_lv1_end_element);
     XML_SetCharacterDataHandler(xmlparser, NULL);
   }
 }
 
-void sharedStrings_rPritem_start_element(void *userData, const XML_Char *name, const XML_Char **attrs) {
+void sharedStrings_rPritem_start_element(void *callbackdata, const XML_Char *name, const XML_Char **attrs) {
   (void)attrs;
   if (strcmp(name, "sz") == 0) {
     for (int i = 0; attrs[i]; i += 2) {
       if (strcmp(attrs[i], "val") == 0) {
-	fontobj.font.sz = strtof((char *)attrs[i + 1], NULL);
+	font.sz = strtof((char *)attrs[i + 1], NULL);
       }
     }
   }
   if (strcmp(name, "rFont") == 0) {
     for (int i = 0; attrs[i]; i += 2) {
       if (strcmp(attrs[i], "val") == 0) {
-	fontobj.font.name = realloc(fontobj.font.name, sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
-	memcpy(fontobj.font.name, attrs[i + 1], sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
+	font.name = realloc(font.name, sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
+	memcpy(font.name, attrs[i + 1], sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
       }
     }
   }
   if (strcmp(name, "b") == 0) {
     for (int i = 0; attrs[i]; i += 2) {
       if (strcmp(attrs[i], "val") == 0) {
-	fontobj.font.isBold = strcmp(attrs[i + 1], "true") == 0 ? '1' : '0';
+	font.isBold = strcmp(attrs[i + 1], "true") == 0 ? '1' : '0';
       }
     }
   }
   if (strcmp(name, "i") == 0) {
     for (int i = 0; attrs[i]; i += 2) {
       if (strcmp(attrs[i], "val") == 0) {
-	fontobj.font.isItalic = strcmp(attrs[i + 1], "true") == 0 ? '1' : '0';
+	font.isItalic = strcmp(attrs[i + 1], "true") == 0 ? '1' : '0';
       }
     }
   }
   if (strcmp(name, "u") == 0) {
     for (int i = 0; attrs[i]; i += 2) {
       if (strcmp(attrs[i], "val") == 0) {
-	fontobj.font.underline = realloc(fontobj.font.underline, sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
-	memcpy(fontobj.font.underline, attrs[i + 1], sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
+	font.underline = realloc(font.underline, sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
+	memcpy(font.underline, attrs[i + 1], sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
       }
     }
   }
   if (strcmp(name, "color") == 0) {
     for (int i = 0; attrs[i]; i += 2) {
       if (strcmp(attrs[i], "val") == 0) {
-	fontobj.font.color.rgb = realloc(fontobj.font.color.rgb, sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
-	memcpy(fontobj.font.color.rgb, attrs[i + 1], sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
+	font.color.rgb = realloc(font.color.rgb, sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
+	memcpy(font.color.rgb, attrs[i + 1], sizeof(XML_Char) * (strlen(attrs[i + 1]) + 1));
       }
     }
   }
-  XML_SetElementHandler(xmlparser, NULL, sharedStrings_rPritem_end_element);
 }
 
-void sharedStrings_rPritem_end_element(void *userData, const XML_Char *name) {
+void sharedStrings_rPritem_end_element(void *callbackdata, const XML_Char *name) {
   XML_SetElementHandler(xmlparser, sharedStrings_rPritem_start_element, sharedStrings_lv2_end_element);
   XML_SetCharacterDataHandler(xmlparser, NULL);
 }
@@ -165,13 +155,14 @@ void sharedStrings_content_handler(void *callbackdata, const XML_Char *buf, int 
   if (len == 0){
     return;
   }
-  FILE *_tmp_sharedStrings_callbackdata = callbackdata;
+
+  FILE *sharedStrings_file_callbackdata = callbackdata;
   XML_Char *value;
   if ((value = malloc(len + 1)) == NULL) {
     return;
   } 
   memcpy(value, buf, len);
   value[len] = '\0';
-  fputs(value, _tmp_sharedStrings_callbackdata);
+  fputs(value, sharedStrings_file_callbackdata);
   free(value);
 }
