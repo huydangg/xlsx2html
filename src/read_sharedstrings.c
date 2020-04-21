a7cc9c670a03465246d5bed0fdae1be01e5e9ccc
dangqhuy <dangqhuy2411@gmail.com>
2020-04-21 17:57:01 +0700

    Clean code

--- src/read_sharedstrings.c
+++ src/read_sharedstrings.c
@@ -18,17 +18,17 @@ struct Font font;
 int current_index;
 
 char* concat(const char *s1, const char *s2) {
-    const size_t len1 = strlen(s1);
-    const size_t len2 = strlen(s2);
-    char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
-    if (result == NULL) {
-      fprintf(stderr, "Cannot concat()");
-      return NULL;
-    }
-    // in real code you would check for errors in malloc here
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
-    return result;
+  const size_t len1 = strlen(s1);
+  const size_t len2 = strlen(s2);
+  char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
+  if (result == NULL) {
+    fprintf(stderr, "Cannot concat()");
+    return NULL;
+  }
+  // in real code you would check for errors in malloc here
+  memcpy(result, s1, len1);
+  memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
+  return result;
 }
 
 void sharedStrings_main_start_element(void *callbackdata, const XML_Char *name, const XML_Char **attrs) {
