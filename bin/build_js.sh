a3c3d29e22acf0493c1dec0eb79d096b701e8666
Huy Dang <dangqhuy2411@gmail.com>
2020-05-13 18:32:47 +0700

    Apply Closure Compiler to optimize js

---
+++ bin/build_js.sh
@@ -0,0 +1,18 @@
+BASEDIR=$1
+CLOSURE_COMPILER_DIR="$BASEDIR/3rdparty/closure-compiler"
+CLOSURE_COMPILER_JAR="$CLOSURE_COMPILER_DIR/compiler.jar"
+INPUT="$BASEDIR/templates/xlsxmagic.js"
+OUTPUT_FN="xlsxmagic.min.js"
+OUTPUT="$BASEDIR/templates/$OUTPUT_FN"
+
+(echo "Building $OUTPUT_FN with closure-compiler..." && \
+    java -jar "$CLOSURE_COMPILER_JAR" \
+         --compilation_level SIMPLE_OPTIMIZATIONS \
+         --warning_level VERBOSE \
+         --output_wrapper "(function(){%output%})();" \
+         --js "$INPUT" \
+         --js_output_file "$OUTPUT" && \
+    echo 'Done.') || \
+(echo 'Failed. Read `3rdparty/closure-compiler/README` for more detail.' && \
+echo 'Using the uncompressed version.' && \
+cat "$INPUT" > "$OUTPUT")
