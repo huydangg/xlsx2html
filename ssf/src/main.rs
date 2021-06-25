6e4dea388aeb0b17dd8d0255593debd306a396e8
Huy Dang <huydang@Huys-MacBook-Pro.local>
2021-06-26 00:32:30 +0700

    WIP: isgeneral

--- ssf/src/main.rs
+++ ssf/src/main.rs
@@ -10,17 +10,79 @@ fn fill(c: &str, l: usize) -> String {
     return o;
 }
 
-fn pad0(v: &str, d: usize) -> String {
+fn pad0(v: f32, d: usize) -> String {
     let mut t = String::new();
-    t.push_str(v);
+    t.push_str(&v.to_string());
     if t.len() >= d {
         return t;
     } else {
         let mut tmp = String::from(fill("0", d-t.len()));
-
-        tmp.push_str(t);
+        tmp.push_str(&t);
+        return tmp;
     }
-    return if t.len() >= d {t} else {fill("0", d-t.len())}
+}
+
+fn pad_(v: f32, d: usize) -> String {
+    let mut t = String::new();
+    t.push_str(&v.to_string());
+    if t.len() >= d {
+        return t;
+    } else {
+        let mut tmp = String::from(fill(" ", d-t.len()));
+        tmp.push_str(&t);
+        return tmp;
+    }
+}
+
+fn rpad_(v: f32, d: usize) -> String {
+    let mut t = String::new();
+    t.push_str(&v.to_string());
+    if t.len() >= d {
+        return t;
+    } else {
+        let tmp = String::from(fill(" ", d-t.len()));
+        t.push_str(&tmp);
+        return t;
+    }
+}
+
+fn pad0r1(v: f32, d: usize) -> String {
+    let mut t = String::new();
+    let tv = v.round() as i16;
+    t.push_str(&tv.to_string());
+    if t.len() >= d {
+        return t;
+    } else {
+        let mut tmp = String::from(fill("0", d-t.len()));
+        tmp.push_str(&t);
+        return tmp;
+    }
+}
+
+fn pad0r2(v: f32, d: usize) -> String {
+    let mut t = String::new();
+    let tv = v.round() as i16;
+    t.push_str(&tv.to_string());
+    if t.len() >= d {
+        return t;
+    } else {
+        let mut tmp = String::from(fill("0", d-t.len()));
+        tmp.push_str(&t);
+        return tmp;
+    }
+}
+
+fn pad0r(v: f32, d:usize) -> String {
+    let mut p2_32 = 2_f32;
+    p2_32 = p2_32.powi(32);
+    if v > p2_32 || v < -p2_32 {
+        return pad0r1(v, d);
+    }
+    let i = v.round();
+    return pad0r2(i, d)
+}
+
+fn isgeneral(s: &str, i: i16) {
 
 }
 
